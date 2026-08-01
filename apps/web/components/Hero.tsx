1338c3d8b4964c3f5f13d8015babd785043682bf
Tanmay Saini <sainitanmay@gmail.com>
2026-08-01 21:10:29 +0000

    chore: initialize HotLap project foundation

---
+++ apps/web/components/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1>Welcome to HotLap</h1>
+
+      <p>
+        Premium RC Cars, Spare Parts, 3D Printed Accessories and RC Events.
+      </p>
+    </section>
+  );
+}
